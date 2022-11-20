515cfe401844c7ead4e4df14ae1dff145d11b8f3
Alexis Brownstein <alexis@drdatabase.com>
2022-11-20 17:28:49 -0500

    started seed.js file logic to load the data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,19 @@
-const sequelize = require('../config/connection');
-const { Traveller, Location, Trip } = require('../models');
+// const sequelize = require('../config/connection');
+// const { Traveller, Location, Trip } = require('../models');
 
-const travellerSeedData = require('./travellerSeedData.json');
-const locationSeedData = require('./locationSeedData.json');
+import sequelize from '../config/connection';
+import {Restaurants, Customers, ItemTypes, Items, OrderTypes, Orders, OrderItems} from '../models';
+
+// const travellerSeedData = require('./travellerSeedData.json');
+// const locationSeedData = require('./locationSeedData.json');
+
+import restaurantsSeedData from './restaurantsSeedData.json';
+import customersSeedData from './customersSeedData.json';
+import itemtypesSeedData from './itemtypesSeedData.json';
+import itemsSeedData from './itemsSeedData.json';
+import ordertypesSeedData from './ordertypesSeedData.json';
+import ordersSeedData from './ordersSeedData.json';
+import orderitemsSeedData from './orderitemsSeedData.json';
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -11,29 +22,7 @@ const seedDatabase = async () => {
 
   const locations = await Location.bulkCreate(locationSeedData);
 
-  // Create trips at random
-  for (let i = 0; i < 10; i++) {
-    // Get a random traveller's `id`
-    const { id: randomTravellerId } = travellers[
-      Math.floor(Math.random() * travellers.length)
-    ];
-
-    // Get a random location's `id`
-    const { id: randomLocationId } = locations[
-      Math.floor(Math.random() * locations.length)
-    ];
-
-    // Create a new trip with random `trip_budget` and `traveller_amount` values, but with ids selected above
-    await Trip.create({
-      trip_budget: (Math.random() * 10000 + 1000).toFixed(2),
-      traveller_amount: Math.floor(Math.random() * 10) + 1,
-      traveller_id: randomTravellerId,
-      location_id: randomLocationId
-    }).catch((err) => {
-      // If there's an error, such as the same random pairing of `traveller.id` and `location.id` occurring and we get a constraint error, don't quit the Node process
-      console.log(err);
-    });
-  }
+  
 
   process.exit(0);
 };
