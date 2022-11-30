754bb2234a1d224cf3943a3cd0fae1eca8a3b45f
Ryan Dong <ryandong@Ryans-MacBook-Pro.local>
2022-11-29 23:15:23 -0800

    all IDS in HTML and dynamic Java file, onclick values update in order

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,13 +1,13 @@
 //buttons to add to `Order`
-var beefAddbtn = document.getElementById("beefAdd");
-var turkeyAddbtn = document.getElementById("turkeyAdd");
-var veggieAddbtn = document.getElementById("veggieAdd");
-var friesAddbtn = document.getElementById("friesAdd");
-var chipsAddbtn = document.getElementById("chipsAdd");
-var sodaAddbtn = document.getElementById("sodaAdd");
-var waterAddbtn = document.getElementById("waterAdd");
-var cookieAddbtn = document.getElementById("cookieAdd");
-var fruitSaladAddbtn = document.getElementById("fruitSaladAdd");
+var beefAddbtn = document.getElementById("beefAdd-1");
+var turkeyAddbtn = document.getElementById("turkeyAdd-2");
+var veggieAddbtn = document.getElementById("veggieAdd-3");
+var friesAddbtn = document.getElementById("friesAdd-4");
+var chipsAddbtn = document.getElementById("chipsAdd-5");
+var sodaAddbtn = document.getElementById("sodaAdd-6");
+var waterAddbtn = document.getElementById("waterAdd-7");
+var cookieAddbtn = document.getElementById("cookieAdd-9");
+var fruitSaladAddbtn = document.getElementById("fruitSaladAdd-8");
 
 
 
@@ -26,7 +26,120 @@ function beefOrder() {
 
 beefAddbtn.addEventListener("click", beefOrder);
 
+function turkeyOrder() {
+    // get quantity from dropdown saves to variable quantity   
+    let quantity = qty2.value;
+ 
+    console.log(quantity);
+  
+    let turkeyTotal = quantity * 5;
+    
+    document.querySelector("#a-2").textContent = quantity;
+    document.querySelector("#t-2").textContent = turkeyTotal;  
+}
 
+turkeyAddbtn.addEventListener("click", turkeyOrder);
+
+function veggieOrder() {
+    // get quantity from dropdown saves to variable quantity   
+    let quantity = qty3.value;
+ 
+    console.log(quantity);
+  
+    let veggieTotal = quantity * 5;
+    
+    document.querySelector("#a-3").textContent = quantity;
+    document.querySelector("#t-3").textContent = veggieTotal;  
+}
+
+veggieAddbtn.addEventListener("click", veggieOrder);
+
+function friesOrder() {
+    // get quantity from dropdown saves to variable quantity   
+    let quantity = qty4.value;
+ 
+    console.log(quantity);
+  
+    let friesTotal = quantity * 2;
+    
+    document.querySelector("#a-4").textContent = quantity;
+    document.querySelector("#t-4").textContent = friesTotal;  
+}
+
+friesAddbtn.addEventListener("click", friesOrder);
+
+function chipsOrder() {
+    // get quantity from dropdown saves to variable quantity   
+    let quantity = qty5.value;
+ 
+    console.log(quantity);
+  
+    let chipsTotal = quantity * 1;
+    
+    document.querySelector("#a-5").textContent = quantity;
+    document.querySelector("#t-5").textContent = chipsTotal;  
+}
+
+chipsAddbtn.addEventListener("click", chipsOrder);
+
+function sodaOrder() { 
+    // get quantity from dropdown saves to variable quantity
+    let quantity = qty6.value;
+
+    console.log(quantity);
+
+    let sodaTotal = quantity * 1.50;
+
+    document.querySelector("#a-6").textContent = quantity;
+    document.querySelector("#t-6").textContent = sodaTotal;
+}
+
+sodaAddbtn.addEventListener("click", sodaOrder);
+
+function waterOrder() {
+    // get quantity from dropdown saves to variable quantity
+    let quantity = qty7.value;
+
+    console.log(quantity);
+
+    let waterTotal = quantity * 1.25;
+
+    document.querySelector("#a-7").textContent = quantity;
+    document.querySelector("#t-7").textContent = waterTotal;
+
+}
+
+waterAddbtn.addEventListener("click", waterOrder);
+       
+function cookieOrder() {
+    // get quantity from dropdown saves to variable quantity
+    let quantity = qty9.value;
+
+    console.log(quantity);
+
+    let cookieTotal = quantity * 2;
+
+    document.querySelector("#a-9").textContent = quantity;
+    document.querySelector("#t-9").textContent = cookieTotal;
+
+}
+
+cookieAddbtn.addEventListener("click", cookieOrder);
+
+function fruitSaladOrder() {
+    // get quantity from dropdown saves to variable quantity
+    let quantity = qty8.value;
+
+    console.log(quantity);
+
+    let fruitSaladTotal = quantity * 2;
+
+    document.querySelector("#a-8").textContent = quantity;
+    document.querySelector("#t-8").textContent = fruitSaladTotal;
+
+}
+
+fruitSaladAddbtn.addEventListener("click", fruitSaladOrder);
 /*
 var qty1El = document.getElementById('qty1');
 
