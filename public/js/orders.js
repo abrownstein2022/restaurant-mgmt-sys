b2a7312d4ac35c262f4f6640fe569f90e3c4dc4f
Ryan Dong <ryandong@Ryans-MacBook-Pro.local>
2022-11-29 22:51:26 -0800

    added IDS and Java functions

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,5 +1,33 @@
+//buttons to add to `Order`
+var beefAddbtn = document.getElementById("beefAdd");
+var turkeyAddbtn = document.getElementById("turkeyAdd");
+var veggieAddbtn = document.getElementById("veggieAdd");
+var friesAddbtn = document.getElementById("friesAdd");
+var chipsAddbtn = document.getElementById("chipsAdd");
+var sodaAddbtn = document.getElementById("sodaAdd");
+var waterAddbtn = document.getElementById("waterAdd");
+var cookieAddbtn = document.getElementById("cookieAdd");
+var fruitSaladAddbtn = document.getElementById("fruitSaladAdd");
 
 
+
+
+function beefOrder() {
+    // get quantity from dropdown saves to variable quantity   
+    let quantity = qty1.value;
+ 
+    console.log(quantity);
+  
+    let beefTotal = quantity * 6;
+    
+    document.querySelector("#a-1").textContent = quantity;
+    document.querySelector("#t-1").textContent = beefTotal;  
+}
+
+beefAddbtn.addEventListener("click", beefOrder);
+
+
+/*
 var qty1El = document.getElementById('qty1');
 
 
@@ -8,3 +36,4 @@ qty1El.addEventListener("click", showIt)
 function showIt() {
     console.log('one');
 }
+*/
